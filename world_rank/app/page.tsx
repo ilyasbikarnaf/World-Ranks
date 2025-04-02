a969d81b05ff57cc221618d7c0685e759e00aedc
ilyasbikarnaf <ilyas.bikarnaf@gmail.com>
2025-04-02 11:20:53 +0000

    configure search input

--- world_rank/app/page.tsx
+++ world_rank/app/page.tsx
@@ -1,12 +1,22 @@
+"use client";
+import searchSVG from "@/assets/Search.svg";
+import Image from "next/image";
+
 export default function Page() {
   return (
-    <div className="bg-blue-400 p-2 w-[95%] mx-auto rounded-lg ">
-      <div>
+    <div className="bg-blue-400 px-2 py-4 w-[95%] mx-auto rounded-lg space-y-3">
+      <div className="flex flex-col gap-y-4">
         <p>Found X countries</p>
 
-        <div>
-          asldkfjlaksjdf
-          <input type="text" />
+        <div className="flex w-full bg-[#282B30] p-2 rounded-lg justify-center">
+          <button>
+            <Image src={searchSVG} alt="search icon" />
+          </button>
+          <input
+            type="text"
+            placeholder="Search by Name, Region..."
+            className="px-2 rounded w-full bg-transparent outline-none"
+          />
         </div>
       </div>
 
